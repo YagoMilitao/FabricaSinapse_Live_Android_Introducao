d3b63eaddc4bc6edd71a26bcbbb70c502b59d7ab
Yago Militao <Yago_LuizMilitao@trimble.com>
2023-01-08 23:07:46 -0300

    Projeto finalizado :)

--- app/src/main/java/com/example/fabricasinapse_live_android_introducao/MainActivity.kt
+++ app/src/main/java/com/example/fabricasinapse_live_android_introducao/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.fabricasinapse_live_android_introducao
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import org.w3c.dom.Text
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("WrongViewCast")
@@ -15,33 +19,89 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var activityResultLauncher: ActivityResultLauncher<*>
+
         val tvResult = findViewById<TextView>(R.id.tvResult)
         val etName = findViewById<EditText>(R.id.etName)
         val btSend = findViewById<Button>(R.id.btSend)
         val btSend2 = findViewById<Button>(R.id.btSend2)
+        val btSend3 = findViewById<Button>(R.id.btSend3)
 
         btSend.setOnClickListener {
-            if(etName.text.isNullOrBlank()){
+            if (etName.text.isNullOrBlank()) {
                 etName.error = getString(R.string.type_your_name)
-            }
-            else{
+            } else {
                 tvResult.text = getString(R.string.hello_name, etName.text.toString())
             }
 
         }
 
         btSend2.setOnClickListener {
-            if(etName.text.isNullOrBlank()){
+            if (etName.text.isNullOrBlank()) {
                 etName.error = getString(R.string.type_your_name)
-            }
-            else{
+            } else {
                 val typedName = etName.text.toString()
 
                 val intent = Intent(this, ResultActivity::class.java)
-                intent.putExtra("TYPED_NAME",typedName)
+                intent.putExtra("TYPED_NAME", typedName)
                 startActivity(intent)
             }
 
         }
+
+        /*fun openActivityForResult() {
+            startForResult.launch(Intent(this, AnotherActivity::class.java))
+        }
+
+        val getAction = registerForActivityResult(ActivityResultContract.StartActivityOnResult()){
+            val result = data.getStringExtra("RESULT")
+            tvResult.text = result
+        }*/
+
+        btSend3.setOnClickListener {
+            val intent = Intent(this, SendResultActivity::class.java)
+
+            val requestCode = 1
+
+
+            startActivityForResult(intent, requestCode)
+
+
+
+
+
+
+        }
+
+        fun onActivityResult(requestCode: Int, result: ActivityResult, resultCode: Int, data: Intent?) {
+            if(requestCode == 1) {
+                if (result.resultCode == Activity.RESULT_OK && data != null) {
+                    val results = data?.getStringExtra("RESULT")
+                    val tvResult = findViewById<TextView>(R.id.tvResult)
+                    tvResult.text = getString(R.string.data_received, results)
+                } else if (result.resultCode == Activity.RESULT_CANCELED){
+                    val tvResult = findViewById<TextView>(R.id.tvResult)
+                    tvResult.text = getString(R.string.activity_was_canceled)
+                }
+
+            }
+        }
+        /*fun onActivityResult(requestCode: Int, result: ActivityResult) {
+            if(result.resultCode == Activity.RESULT_OK && requestCode == 1) {
+                val result = result.data?.getStringExtra("RESULT")
+                val tvResult = findViewById<TextView>(R.id.tvResult)
+                tvResult.text = result
+
+            }*/
+
+        /*fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null){
+                val result = data.getStringExtra("RESULT")
+
+
+            }
+        }*/
     }
+
 }
--- app/src/main/java/com/example/fabricasinapse_live_android_introducao/SendResultActivity.kt
+++ app/src/main/java/com/example/fabricasinapse_live_android_introducao/SendResultActivity.kt
@@ -1,8 +1,11 @@
 package com.example.fabricasinapse_live_android_introducao
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.annotation.StringRes
 
 class SendResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +14,30 @@ class SendResultActivity : AppCompatActivity() {
 
         val btYes = findViewById<Button>(R.id.btYes)
         val btNo = findViewById<Button>(R.id.btNo)
-        
+        val btCancel = findViewById<Button>(R.id.btCancel)
+
+        btYes.setOnClickListener {
+            sendResult(R.string.yes)
+        }
+
+        btNo.setOnClickListener {
+            sendResult(R.string.no)
+        }
+
+        btCancel.setOnClickListener {
+            setResult((Activity.RESULT_CANCELED))
+
+            finish()
+        }
+
+    }
+
+    private fun sendResult(@StringRes stringResID: Int) {
+        val intent = Intent()
+        intent.putExtra("RESULT", getString(stringResID))
+
+        setResult(RESULT_OK, intent)
+
+        finish()
     }
 }
